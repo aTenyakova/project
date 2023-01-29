2770b0ab2295d56ebc1dd136ace97a6ab30cc17c
Alena <alena.teniakova@gmail.com>
2023-01-30 01:03:24 +0700

    метод печати массива

--- Program.cs
+++ Program.cs
@@ -15,3 +15,16 @@ void GetArray(string[] arrayOne, string[] arrayTwo)
         }
     }
 }
+
+GetArray(arrayOne, arrayTwo);
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(arrayTwo);
