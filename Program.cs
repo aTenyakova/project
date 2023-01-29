f132476719fdd29a189af71ba3b90a41b4356f8f
Alena <alena.teniakova@gmail.com>
2023-01-30 01:01:43 +0700

    метод нахождения подходящих строк

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 
 string[] arrayOne = new string[5] {"1", "12", "123", "1234", "12345"};
 string[] arrayTwo = new string[arrayOne.Length];
+
+void GetArray(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length < 4)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
